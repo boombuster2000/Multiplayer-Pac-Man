259d0f974b84f628b7d831ba4fe283b62bd622f5
boombuster2000 <chanuka.williams@outlook.com>
2025-10-05 22:34:19 +0100

    created enums

---
+++ Core/Source/Components/Enums.h
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace Components
+{
+    enum AnchorPoint
+    {
+        TOP_LEFT,
+        TOP_MIDDLE,
+        TOP_RIGHT,
+
+        MIDDLE_LEFT,
+        MIDDLE,
+        MIDDLE_RIGHT,
+
+        BOTTOM_LEFT,
+        BOTTOM_MIDDLE,
+        BOTTOM_RIGHT
+    };
+    
+    enum Alignment
+    {
+        LEFT,
+        CENTER,
+        RIGHT
+    };
+}
+
