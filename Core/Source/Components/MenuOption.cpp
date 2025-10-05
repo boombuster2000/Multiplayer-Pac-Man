e6d19075c379f9c070460490fbea19f4c9d6ae54
boombuster2000 <chanuka.williams@outlook.com>
2025-10-05 22:34:59 +0100

    Created class for menu options

---
+++ Core/Source/Components/MenuOption.cpp
@@ -0,0 +1,34 @@
+#include "MenuOption.h"
+
+namespace Components {
+
+    MenuOption::MenuOption(std::string text,
+            TextStyle selectedStyle,
+            TextStyle unselectedStyle,
+            bool visible,
+            bool isSelected
+    ): Text(text, unselectedStyle, {0,0}, TOP_LEFT, visible),
+       m_selectedStyle(selectedStyle), m_unselectedStyle(unselectedStyle), m_isSelected(isSelected)
+    {
+    }
+
+    void MenuOption::SetSelected(bool isSelected)
+    {
+        m_isSelected = isSelected;
+
+        if (m_isSelected)
+        {
+            SetStyle(m_selectedStyle);
+        }
+        else 
+        {
+            SetStyle(m_unselectedStyle);
+        }
+    }
+
+    bool MenuOption::IsSelected() const
+    {
+        return m_isSelected;
+    }
+
+}
---
+++ Core/Source/Components/MenuOption.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Text.h"
+#include "TextStyle.h"
+#include "Enums.h"
+#include "RenderableObject.h"
+#include <string>
+
+
+namespace Components {
+
+    
+	class MenuOption : public Text
+	{
+
+    private:
+        bool m_isSelected;
+        TextStyle m_selectedStyle;
+        TextStyle m_unselectedStyle;
+    
+	public:
+        
+        MenuOption(std::string text,
+            TextStyle selectedStyle,
+            TextStyle unselectedStyle,
+            bool visible,
+            bool isSelected
+        );
+
+		~MenuOption() = default;
+
+        void SetSelected(bool selected);
+        bool IsSelected() const;
+	};
+
+}
