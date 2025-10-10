d2eaa146463b79af916e94883ae9fc5272deb641
boombuster200 <chanuka.williams@outlook.com>
2025-10-10 09:14:22 +0100

    Created function to set tiles.

--- App/Source/GameComponents/Board.h
+++ App/Source/GameComponents/Board.h
@@ -1,14 +1,23 @@
 #pragma once
 #include "Tile.h"
 #include <array>
+#include "raylib.h"
 
 class Board
 {
     private:
+    Vector2 m_tileDimensions;
+    Vector2 m_boardDimensions;
     std::array<std::array<Tile, 8>, 8> m_board;
 
+    
     public:
-    Board(std::array<std::array<Tile::Type, 8>, 8> board);
+    Board();
+
+    void SetTile(Vector2 position, Tile::Type type);
 
     void Render() const;
+
+    private:
+    void AddBoundaries();
 };
