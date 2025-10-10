3fa08ffd5aaeb051c180ec9b62eaca9bbabd667c
boombuster200 <chanuka.williams@outlook.com>
2025-10-10 09:13:21 +0100

    Textures is all loaded in as application starts.

--- App/Source/Main.cpp
+++ App/Source/Main.cpp
@@ -3,6 +3,13 @@
 #include "MainMenuComponents/MainMenuLayer.h"
 #include "GameComponents/GameLayer.h"
 
+void LoadAllTextures()
+{
+	Core::Application::GetTexturesManager()->AddTexture("wall", "./Resources/Textures/Wall.png");
+    Core::Application::GetTexturesManager()->AddTexture("path", "./Resources/Textures/Path.png");
+    Core::Application::GetTexturesManager()->AddTexture("pac-man", "./Resources/Textures/Pac-Man.png");
+}
+
 int main()
 {
 	Core::ApplicationSpecification appSpec;
@@ -11,6 +18,7 @@ int main()
 	appSpec.Height = 720;
 
 	Core::Application application(appSpec);
+	LoadAllTextures();
 
 	application.PushLayer<GameLayer>();
 	application.Run();
