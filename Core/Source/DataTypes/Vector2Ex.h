587d92996e55d836657ea587fd3d3e40988f6096
boombuster200 <chanuka.williams@outlook.com>
2025-10-28 13:26:13 +0000

    Added support for divide operation with an integer.

--- Core/Source/DataTypes/Vector2Ex.h
+++ Core/Source/DataTypes/Vector2Ex.h
@@ -1,8 +1,9 @@
 #pragma once
-#include "raylib.h"
 #include <concepts>
 #include <cmath>
 #include <type_traits>
+#include "raylib.h"
+#include "UIComponents/Enums.h"
 
 template <typename T>
 concept NumberLike = requires(T a, T b) {
@@ -12,6 +13,10 @@ concept NumberLike = requires(T a, T b) {
     { a / b } -> std::convertible_to<T>;
     { std::sqrt(a) } -> std::convertible_to<T>;
     { a == b } -> std::convertible_to<bool>;
+    { a++ } -> std::convertible_to<T>;
+    { ++a } -> std::convertible_to<T>;
+    { b-- } -> std::convertible_to<T>;
+    { --b } -> std::convertible_to<T>;
 };
 
 template <NumberLike T>
@@ -60,5 +65,33 @@ public:
     constexpr Vector2Ex operator/(const Vector2Ex &o) const
     {
         return {x / o.x, y / o.y};
+    }
+
+    constexpr Vector2Ex operator/(const int &o) const
+    {
+        return {x / o, y / o};
+    }
+
+    // get Vector offsets funcs
+
+    Vector2Ex<T> GetShiftedVector(const UIComponents::Direction &directionOffset, const Vector2Ex<T> &offset) const
+    {
+        using enum UIComponents::Direction;
+        const T &xOffset = offset.x;
+        const T &yOffset = offset.y;
+
+        switch (directionOffset)
+        {
+        case UP:
+            return Vector2Ex<T>(x, y - yOffset);
+        case DOWN:
+            return Vector2Ex<T>(x, y + yOffset);
+        case LEFT:
+            return Vector2Ex<T>(x - xOffset, y);
+        case RIGHT:
+            return Vector2Ex<T>(x + xOffset, y);
+        default:
+            return *this;
+        }
     }
 };
