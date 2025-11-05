f772fd71270b91314cbdc888abe4f8f2dfe31df5
boombuster2000 <chanuka.williams@outlook.com>
2025-11-05 21:41:02 +0000

    Changed collision system so that it checks intermediate steps to prevent tunneling through walls at fast speeds.

--- App/Source/GameComponents/GameLayer.cpp
+++ App/Source/GameComponents/GameLayer.cpp
@@ -7,53 +7,87 @@
 #include <stdexcept>
 #include <array>
 
-bool GameLayer::ShouldPacmanMove(const UIComponents::Direction direction) const
+Vector2Ex<int> GameLayer::GetNextValidPacmanPosition(const UIComponents::Direction direction) const
 {
     using namespace UIComponents;
     using enum Direction;
 
+    const Vector2Ex<int> currentPosition = m_pacman.GetPositionAtAnchor();
     const Vector2Ex<int> nextPosition = m_pacman.GetNextPosition(direction);
 
-    const int offset = 1; // Small offset to check corners within the hitbox
-    Vector2Ex<int> cornersToCheck[2];
-    const Vector2Ex<int> pacmanDimensions = m_pacman.GetDimensions();
+    const Vector2Ex<int> movementDelta = nextPosition - currentPosition;
 
-    switch (direction)
+    int steps;
+
+    if (direction == UP || direction == DOWN)
+        steps = std::abs(movementDelta.y);
+    else
+        steps = std::abs(movementDelta.x);
+
+    Vector2Ex<int> nextValidPosition = currentPosition;
+    for (int step = 0; step <= steps; step++)
     {
-    case UP:
-        cornersToCheck[0] = nextPosition + Vector2Ex<int>(offset, offset);
-        cornersToCheck[1] = nextPosition + Vector2Ex<int>(pacmanDimensions.x, 0) + Vector2Ex<int>(-offset, offset);
-        break;
+        Vector2Ex<int> intermediatePosition;
+        if (steps == 0)
+            intermediatePosition = currentPosition;
+        else
+            intermediatePosition = currentPosition + (movementDelta / steps) * step;
 
-    case DOWN:
-        cornersToCheck[0] = nextPosition + Vector2Ex<int>(0, pacmanDimensions.y) + Vector2Ex<int>(offset, -offset);
-        cornersToCheck[1] = nextPosition + pacmanDimensions + Vector2Ex<int>(-offset, -offset);
-        break;
+        Vector2Ex<int> cornersToCheck[2];
+        const Vector2Ex<int> pacmanDimensions = m_pacman.GetDimensions();
 
-    case LEFT:
-        cornersToCheck[0] = nextPosition + Vector2Ex<int>(1, 1);
-        cornersToCheck[1] = nextPosition + Vector2Ex<int>(0, pacmanDimensions.y) + Vector2Ex<int>(offset, -offset);
-        break;
+        switch (direction)
+        {
+        case UP:
+            cornersToCheck[0] = intermediatePosition;
+            cornersToCheck[1] = intermediatePosition + Vector2Ex<int>(pacmanDimensions.x, 0);
+            break;
 
-    case RIGHT:
-        cornersToCheck[0] = nextPosition + Vector2Ex<int>(pacmanDimensions.x, 0) + Vector2Ex<int>(-offset, offset);
-        cornersToCheck[1] = nextPosition + pacmanDimensions + Vector2Ex<int>(-offset, -offset);
-        break;
+        case DOWN:
+            cornersToCheck[0] = intermediatePosition + Vector2Ex<int>(0, pacmanDimensions.y);
+            cornersToCheck[1] = intermediatePosition + Vector2Ex<int>(pacmanDimensions.x, pacmanDimensions.y);
+            break;
+
+        case LEFT:
+            cornersToCheck[0] = intermediatePosition;
+            cornersToCheck[1] = intermediatePosition + Vector2Ex<int>(0, pacmanDimensions.y);
+            break;
+
+        case RIGHT:
+            cornersToCheck[0] = intermediatePosition + Vector2Ex<int>(pacmanDimensions.x, 0);
+            cornersToCheck[1] = intermediatePosition + Vector2Ex<int>(pacmanDimensions.x, pacmanDimensions.y);
+            break;
+        }
+
+        bool hasHitWall = false;
+        for (auto corner : cornersToCheck)
+        {
+            // This is done because the tile top right corner position is the exact same as the top left corner position of the next tile.
+            if (direction == LEFT)
+                corner.x -= 1;
+            else if (direction == UP)
+                corner.y -= 1;
+
+            const Tile &tileAtCorner = m_board.GetTileFromPosition(corner);
+            if (tileAtCorner.GetType() == Tile::Type::Wall)
+            {
+                hasHitWall = true;
+                break;
+            }
+        }
+
+        if (hasHitWall)
+            return nextValidPosition;
+        else
+            nextValidPosition = intermediatePosition;
     }
 
-    for (const auto &corner : cornersToCheck)
-    {
-        const Tile &tileAtCorner = m_board.GetTileFromPosition(corner); // Slight offset to ensure we are within the tile
-        if (tileAtCorner.GetType() == Tile::Type::Wall)
-            return false;
-    }
-
-    return true;
+    return nextValidPosition;
 }
 
 GameLayer::GameLayer()
     : m_board(Board()),
-      m_pacman(m_board.GetPositionFromIndex(Vector2Ex<int>(1, 1)), Vector2Ex<int>(50, 50), 5)
+      m_pacman(m_board.GetPositionFromIndex(Vector2Ex<int>(1, 1)) + Vector2Ex<int>(1, 1), Vector2Ex<int>(48, 48), 5)
 {
 }
 
@@ -76,11 +110,13 @@ void GameLayer::HandleKeyPresses()
 void GameLayer::HandleCollisions()
 {
 
-    if (ShouldPacmanMove(m_pacman.GetQueuedDirection()))
+    const Vector2Ex<int> currentPosition = m_pacman.GetPositionAtAnchor();
+    const Vector2Ex<int> nextQueuedPosition = GetNextValidPacmanPosition(m_pacman.GetQueuedDirection());
+
+    if (currentPosition != nextQueuedPosition)
         m_pacman.ApplyQueuedDirection();
 
-    if (ShouldPacmanMove(m_pacman.GetCurrentDirection()))
-        m_pacman.UpdatePosition();
+    m_pacman.SetPosition(GetNextValidPacmanPosition(m_pacman.GetCurrentDirection()));
 }
 
 void GameLayer::OnUpdate(float ts)
