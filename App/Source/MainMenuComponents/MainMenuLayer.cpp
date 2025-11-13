ccc084d7e81f541ce82d6bf1bd4781b50687aa23
boombuster2000 <chanuka.williams@outlook.com>
2025-11-13 18:06:38 +0000

    Can now transition to game layer.

--- App/Source/MainMenuComponents/MainMenuLayer.cpp
+++ App/Source/MainMenuComponents/MainMenuLayer.cpp
@@ -2,6 +2,7 @@
 #include "UIComponents/Enums.h"
 #include "UIComponents/TextStyle.h"
 #include "Core/Application.h"
+#include "GameComponents/GameLayer.h"
 #include <iostream>
 
 MainMenuLayer::MainMenuLayer()
@@ -11,9 +12,14 @@ MainMenuLayer::MainMenuLayer()
 	TextStyle unselectedStyle = {50, BLACK};
 	TextStyle selectedStyle = {60, RED};
 
-	m_menu.AddOption(MenuOption("Start Game", selectedStyle, unselectedStyle, true, true, []() { std::cout << "Start Game" << std::endl; }));
-	m_menu.AddOption(MenuOption("Options", selectedStyle, unselectedStyle, true, false, []() { std::cout << "Options" << std::endl; }));
-	m_menu.AddOption(MenuOption("Exit", selectedStyle, unselectedStyle, true, false, []() { Core::Application::Get().Stop(); }));
+	m_menu.AddOption(MenuOption("Start Game", selectedStyle, unselectedStyle, true, true, []()
+								{ Core::Application::QueueTransition<MainMenuLayer, GameLayer>(); }));
+
+	m_menu.AddOption(MenuOption("Options", selectedStyle, unselectedStyle, true, false, []()
+								{ std::cout << "Options" << std::endl; }));
+
+	m_menu.AddOption(MenuOption("Exit", selectedStyle, unselectedStyle, true, false, []()
+								{ Core::Application::Get().Stop(); }));
 }
 
 MainMenuLayer::~MainMenuLayer()
