c7a6b81323a519c5a82d01fcad9332043f7ffb7b
boombuster200 <chanuka.williams@outlook.com>
2025-11-13 14:42:52 +0000

    Added callbacks for options.

--- App/Source/MainMenuComponents/MainMenuLayer.cpp
+++ App/Source/MainMenuComponents/MainMenuLayer.cpp
@@ -1,17 +1,19 @@
 #include "MainMenuLayer.h"
 #include "UIComponents/Enums.h"
 #include "UIComponents/TextStyle.h"
+#include "Core/Application.h"
+#include <iostream>
 
 MainMenuLayer::MainMenuLayer()
 	: m_menu({(float)GetScreenWidth() / 2, (float)GetScreenHeight() / 2}, UIComponents::AnchorPoint::MIDDLE, UIComponents::Alignment::CENTER, true, 10)
 {
+	using namespace UIComponents;
+	TextStyle unselectedStyle = {50, BLACK};
+	TextStyle selectedStyle = {60, RED};
 
-	UIComponents::TextStyle unselectedStyle = {50, BLACK};
-	UIComponents::TextStyle selectedStyle = {60, RED};
-
-	m_menu.AddOption(UIComponents::MenuOption("Start Game", selectedStyle, unselectedStyle, true, true));
-	m_menu.AddOption(UIComponents::MenuOption("Options", selectedStyle, unselectedStyle, true, false));
-	m_menu.AddOption(UIComponents::MenuOption("Exit", selectedStyle, unselectedStyle, true, false));
+	m_menu.AddOption(MenuOption("Start Game", selectedStyle, unselectedStyle, true, true, []() { std::cout << "Start Game" << std::endl; }));
+	m_menu.AddOption(MenuOption("Options", selectedStyle, unselectedStyle, true, false, []() { std::cout << "Options" << std::endl; }));
+	m_menu.AddOption(MenuOption("Exit", selectedStyle, unselectedStyle, true, false, []() { Core::Application::Get().Stop(); }));
 }
 
 MainMenuLayer::~MainMenuLayer()
@@ -27,6 +29,10 @@ void MainMenuLayer::OnUpdate(float ts)
 	else if (IsKeyPressed(KEY_UP))
 	{
 		m_menu.SelectPrevious();
+	}
+	else if (IsKeyPressed(KEY_ENTER))
+	{
+		m_menu.ConfirmSelection();
 	}
 
 	if (m_menu.IsUIUpdateNeeded())
