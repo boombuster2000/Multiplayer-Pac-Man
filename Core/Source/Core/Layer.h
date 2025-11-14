996a784df67c3902d8403f6bdbd26440f1f9db48
boombuster200 <chanuka.williams@outlook.com>
2025-11-14 09:27:57 +0000

    Created states for layers.

--- Core/Source/Core/Layer.h
+++ Core/Source/Core/Layer.h
@@ -8,12 +8,29 @@ namespace Core
 	class Layer
 	{
 	public:
+		enum class State
+		{
+			Active,
+			Suspended_Update,
+			Suspended_UpdateAndRender,
+		};
+
+	public:
 		virtual ~Layer() = default;
 
 		virtual void OnEvent(Event &event) {}
 
 		virtual void OnUpdate(float ts) {}
 		virtual void OnRender() {}
+
+		void SuspendUpdate() { m_State = State::Suspended_Update; }
+		void SuspendUpdateAndRender() { m_State = State::Suspended_UpdateAndRender; }
+		void Resume() { m_State = State::Active; }
+
+		State GetState() const { return m_State; }
+
+	protected:
+		State m_State = State::Active;
 	};
 
 }
