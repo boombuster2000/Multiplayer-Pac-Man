e6a8cc27881c2960539d56f41c39e45576892f95
boombuster2000 <chanuka.williams@outlook.com>
2025-11-23 22:29:24 +0000

    name is now serialised.

--- game/include/game/serialization/board_json.hpp
+++ game/include/game/serialization/board_json.hpp
@@ -5,12 +5,21 @@
 
 using json = nlohmann::json;
 
-inline void to_json(json& j, const Board& board) {
+inline void to_json(json& j, const Board& board)
+{
     const auto& grid = static_cast<const ui::Grid<Tile>&>(board);
     to_json(j, grid);
+
+    j["name"] = board.m_name;
 }
 
-inline void from_json(const json& j, Board& board) {
+inline void from_json(const json& j, Board& board)
+{
     auto& grid = static_cast<ui::Grid<Tile>&>(board);
     from_json(j, grid);
+
+    if (!j.contains("name"))
+        throw std::runtime_error("Failed to deserialize Board: missing field 'name'");
+
+    j.at("name").get_to(board.m_name);
 }
