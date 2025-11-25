03f5f5cf6e765144f04cf0c57f0fcf456136f752
boombuster2000 <chanuka.williams@outlook.com>
2025-11-25 00:02:46 +0000

    Removed uneeded deconstructor definition.

--- engine/include/engine/ui/grid_tile.h
+++ engine/include/engine/ui/grid_tile.h
@@ -15,7 +15,6 @@ class GridTile : public RenderableObject
   public:
     GridTile();
     GridTile(Vector2Ex<float> dimensions, Vector2Ex<float> position);
-    ~GridTile() = default;
 
     virtual Vector2Ex<float> GetDimensions() const override;
 
