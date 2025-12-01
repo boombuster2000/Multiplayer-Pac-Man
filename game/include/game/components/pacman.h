62479f18f68b34b264b7a8abdf5dcf306b10cdbc
boombuster2000 <chanuka.williams@outlook.com>
2025-12-01 13:58:54 +0000

    Pacman now inherits entity class.

--- game/include/game/components/pacman.h
+++ game/include/game/components/pacman.h
@@ -1,46 +1,25 @@
 #pragma once
 #include "engine/core/vector2ex.h"
 #include "engine/ui/enums.h"
-#include "engine/ui/renderable_object.h"
+#include "game/components/entity.h"
 #include "raylib.h"
 #include <memory>
 
-class Pacman : public ui::RenderableObject
+class Pacman : public Entity
 {
-  private:
-    using enum ui::Direction;
-    std::shared_ptr<Texture2D> m_texture;
-    Vector2Ex<float> m_spawnPosition;
-    Vector2Ex<float> m_dimensions;
-    float m_speed;
-    float m_Rotation;
-    Vector2Ex<float> m_lastPosition;
-    ui::Direction m_currentDirection;
-    ui::Direction m_queuedDirection;
 
   private:
-    void SetRotation(const ui::Direction& direction);
+    Vector2Ex<float> m_lastPosition;
+
+  private:
+    float ConvertDirectionToRotation(const ui::Direction& direction) const;
 
   public:
-    Pacman();
-    Pacman(Vector2Ex<float> spawnPosition, Vector2Ex<float> dimensions, float Speed);
-
-    ui::Direction GetCurrentDirection() const;
-    ui::Direction GetQueuedDirection() const;
-
-    void QueueDirection(ui::Direction direction);
-    void ApplyQueuedDirection();
+    Pacman(Vector2Ex<float> spawnPosition, Vector2Ex<float> dimensions, float speed);
 
     void SetPosition(const Vector2Ex<float> position) final;
-    Vector2Ex<float> GetNextPosition(const ui::Direction& direction, const float& deltaTime) const;
-    Vector2Ex<float> GetNextPositionWithStep(const ui::Direction& direction, const float step) const;
-
-    float GetSpeed() const;
+    void SetQueuedDirection(const ui::Direction& direction) final;
+    void ApplyQueuedDirection() final;
 
     bool IsStationary() const;
-
-    Vector2Ex<float> GetDimensions() const final;
-    void SetDimensions(const Vector2Ex<float>& dimensions);
-
-    void Render(Vector2Ex<float> offset = {0, 0}) const final;
 };
--- game/src/components/pacman.cpp
+++ game/src/components/pacman.cpp
@@ -1,174 +1,53 @@
 #include "game/components/pacman.h"
 #include "game/game_application.h"
 
-Pacman::Pacman() = default;
-
-void Pacman::SetRotation(const ui::Direction& direction)
+float Pacman::ConvertDirectionToRotation(const ui::Direction& direction) const
 {
+    using enum ui::Direction;
     switch (direction)
     {
     case UP:
-        m_Rotation = -90;
-        break;
+        return 270.0f;
     case DOWN:
-        m_Rotation = 90;
-        break;
+        return 90.0f;
     case LEFT:
-        m_Rotation = 180;
-        break;
+        return 180.0f;
     case RIGHT:
-        m_Rotation = 0;
-        break;
+        return 0.0f;
     default:
-        break;
+        return 0.0f;
     }
 }
 
 Pacman::Pacman(Vector2Ex<float> spawnPosition, Vector2Ex<float> dimensions, float speed) :
-    RenderableObject(spawnPosition),
-    m_texture(game::GameApplication::Get().GetTexturesManager().GetTexture("pac-man")),
-    m_spawnPosition(spawnPosition),
-    m_dimensions(dimensions),
-    m_speed(speed),
-    m_Rotation(0),
-    m_lastPosition(spawnPosition),
-    m_currentDirection(LEFT),
-    m_queuedDirection(m_currentDirection)
+    Entity(spawnPosition,
+           Vector2Ex<float>(speed, speed),
+           dimensions,
+           ui::Direction::LEFT,
+           game::GameApplication::Get().GetTexturesManager().GetTexture("pac-man"))
 {
-}
-
-ui::Direction Pacman::GetCurrentDirection() const
-{
-    return m_currentDirection;
-}
-
-ui::Direction Pacman::GetQueuedDirection() const
-{
-    return m_queuedDirection;
-}
-
-void Pacman::QueueDirection(ui::Direction direction)
-{
-    m_queuedDirection = direction;
-
-    if (IsStationary())
-        SetRotation(m_queuedDirection);
-}
-
-void Pacman::ApplyQueuedDirection()
-{
-    m_currentDirection = m_queuedDirection;
-    SetRotation(m_currentDirection);
 }
 
 void Pacman::SetPosition(const Vector2Ex<float> position)
 {
     m_lastPosition = GetWorldOrigin();
-    RenderableObject::SetPosition(position);
+    ui::RenderableObject::SetPosition(position);
 }
 
-Vector2Ex<float> Pacman::GetNextPosition(const ui::Direction& direction, const float& deltaTime) const
+void Pacman::SetQueuedDirection(const ui::Direction& direction)
 {
-    using enum ui::Direction;
-    Vector2Ex<float> nextPosition = GetPositionAtAnchor();
-
-    switch (direction)
-    {
-    case UP:
-        nextPosition.y -= m_speed * deltaTime;
-        break;
-
-    case DOWN:
-        nextPosition.y += m_speed * deltaTime;
-        break;
-
-    case LEFT:
-        nextPosition.x -= m_speed * deltaTime;
-        break;
-
-    case RIGHT:
-        nextPosition.x += m_speed * deltaTime;
-        break;
-
-    default:
-        break;
-    }
-
-    return nextPosition;
+    Entity::SetQueuedDirection(direction);
+    if (IsStationary())
+        SetRotation(ConvertDirectionToRotation(GetQueuedDirection()));
 }
 
-Vector2Ex<float> Pacman::GetNextPositionWithStep(const ui::Direction& direction, const float step) const
+void Pacman::ApplyQueuedDirection()
 {
-    using enum ui::Direction;
-    Vector2Ex<float> nextPosition = GetPositionAtAnchor();
-
-    switch (direction)
-    {
-    case UP:
-        nextPosition.y -= step;
-        break;
-
-    case DOWN:
-        nextPosition.y += step;
-        break;
-
-    case LEFT:
-        nextPosition.x -= step;
-        break;
-
-    case RIGHT:
-        nextPosition.x += step;
-        break;
-
-    default:
-        break;
-    }
-
-    return nextPosition;
-}
-
-float Pacman::GetSpeed() const
-{
-    return m_speed;
+    Entity::ApplyQueuedDirection();
+    SetRotation(ConvertDirectionToRotation(GetDirection()));
 }
 
 bool Pacman::IsStationary() const
 {
     return GetWorldOrigin() == m_lastPosition;
-}
-
-Vector2Ex<float> Pacman::GetDimensions() const
-{
-    return m_dimensions;
-}
-
-void Pacman::SetDimensions(const Vector2Ex<float>& dimensions)
-{
-    m_dimensions = dimensions;
-}
-
-void Pacman::Render(Vector2Ex<float> offset) const
-{
-    using enum ui::Direction;
-    using enum ui::AnchorPoint;
-
-    const float scale = m_dimensions.y / static_cast<float>(m_texture->height);
-
-    const Vector2Ex<float> centerPosition = GetPositionAtAnchor(MIDDLE);
-
-    // Define source and destination rectangles
-
-    const float& textureWidth = static_cast<float>(m_texture->width);
-    const float& textureHeight = static_cast<float>(m_texture->height);
-
-    Rectangle sourceRect = {0.0f, 0.0f, textureWidth, textureHeight};
-    Rectangle destRect = {centerPosition.x + offset.x,
-                          centerPosition.y + offset.y,
-                          m_texture->width * scale,
-                          m_texture->height * scale};
-
-    // Center of rotation (pivot)
-    Vector2 origin = {(textureWidth * scale) / 2.0f, (textureHeight * scale) / 2.0f};
-
-    DrawTexturePro(*m_texture, sourceRect, destRect, origin, m_Rotation, WHITE);
 }
