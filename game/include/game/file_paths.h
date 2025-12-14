a4eb7db75c76ccf79d339d45a74b56a7cf5f5c45
boombuster2000 <chanuka.williams@outlook.com>
2025-12-14 18:03:34 +0000

    Added file path for music

--- game/include/game/file_paths.h
+++ game/include/game/file_paths.h
@@ -7,4 +7,5 @@ struct FilePaths
     inline static const std::filesystem::path s_resourceDirectory = "resources";
     inline static const std::filesystem::path s_boardsDirectory = s_resourceDirectory / "boards";
     inline static const std::filesystem::path s_texturesDirectory = s_resourceDirectory / "textures";
+    inline static const std::filesystem::path s_musicDirectory = s_resourceDirectory / "music";
 };
